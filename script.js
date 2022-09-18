a06487b12d00c239d3bad42bf2ffa11ec3f5ef78
Necryl <necrylthewayfarer@gmail.com>
2022-09-18 18:25:36 +0530

    FINISH ALMOST ALL OF THE JS

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 const libraryElement = document.querySelector('.library');
 const sampleBookElement = document.querySelector('.sample > .book');
 const newBtnElement = document.querySelector('.newBtn');
+const statsElements = [...document.querySelectorAll('.stats>*')];
 
 // variables
 let library = []
 
 // events
 newBtnElement.addEventListener('click', event => {
-    doIt();
+    addBookToLibrary();
 });
 
 // constructor
@@ -17,6 +18,9 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.element = createBookElem(title, author, pages, read);
+
+    libraryElement.appendChild(this.element);
 
     this.info = () => {
         return [this.title, this.author, this.pages, this.read?'read':'not read yet'].join(', ');
@@ -25,17 +29,97 @@ function Book(title, author, pages, read) {
 
 // functions
 function addBookToLibrary() {
+    let params = [...arguments];
+    if (params.length === 0) {
+        params = askBookDetails();
+    }
+    library.push(new Book(...params));
+    updateStats();
+}
+
+function addSampleBook(num=1) {
+    for (let i = 0; i < num; i++) {
+        addBookToLibrary('Sample Title', 'Author Name', 'x', false);
+    }
+}
+
+function createBookElem(title, author, pages, read) {
+    let result = sampleBookElement.cloneNode(true);
+
+    result.childNodes[1].setAttribute('libIndex', `${library.length}`);
+    result.childNodes[1].addEventListener('click', event => {
+        removeBook(event.target.getAttribute('libIndex'));
+    })
+    result.childNodes[3].textContent = title;
+    result.childNodes[5].textContent = author;
+    result.childNodes[7].textContent = `Pages: ${pages}`;
+    let readSpan = result.childNodes[9].childNodes[1];
+    readSpan.setAttribute('libIndex', `${library.length}`);
+    toggleRead(readSpan, read);
+    readSpan.addEventListener('click', event => {
+        toggleRead(event.target);
+    })
+
+    return result;
+}
+
+function toggleRead(elem, read=null, newElem=false) {
+    if (read === null) {
+        read = elem.textContent === 'Yes' ? false:true;
+    } else {
+        newElem = true;
+    }
+    if (!newElem) {
+        library[elem.getAttribute('libIndex')].read = read;
+    }
+    if (read) {
+        elem.classList.add('read');
+        read = 'Yes';
+    } else {
+        elem.classList.remove('read');
+        read = 'Not yet';
+    }
+    elem.textContent = read;
+    updateStats();
+}
+
+function askBookDetails() {
     let params = [];
     let prompts = ['Book title:', 'Book author:', 'Number of pages']
     for (let i = 0; i < 3; i++) {
         params.push(prompt(prompts[i]));
     }
     params.push(confirm('If you have finished reading this book then press OK, otherwise press Cancel'));
-    library.push(new Book(...params));
+    
+    return params;
 }
 
-function doIt(num=1) {
-    for (let i = 0; i < num; i++) {
-        libraryElement.appendChild(sampleBookElement.cloneNode(true));
-    }
+function removeBook(index) {
+    library[index].element.remove();
+    library.splice(index, 1);
+    library.forEach((book, index) => {
+        book.element.childNodes[1].setAttribute('libIndex', index);
+        book.element.childNodes[9].childNodes[1].setAttribute('libIndex', index);
+    });
+    updateStats();
 }
+
+function updateStats() {
+    let stats = [library.length, 0, 0];
+
+    library.forEach(book => {
+        if (book.read) {
+            stats[1]++;
+        } else {
+            stats[2]++;
+        }
+    })
+
+    statsElements.forEach((element, index) => {
+        element.textContent = element.textContent.slice(0, element.textContent.length-1) + stats[index];
+    });
+}
+
+
+//on start
+addSampleBook();
